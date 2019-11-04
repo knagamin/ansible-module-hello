9cd72519abdaf89de159efa0c5f8baee0d03bbc0
Kiyo Nagamine <knagamin.hub@gmail.com>
2019-11-04 22:12:21 +0900

    changed: implemented simple hello module

--- library/hello.py
+++ library/hello.py
@@ -2,31 +2,19 @@
 
 from ansible.module_utils.basic import AnsibleModule
 
-DOCUMENTATION = '''
-module: hello
-short_descriptioin: "Make a greeting"
-'''
-
 
 def run_module():
-    module_args = dict(
-        name=dict(required=True, type='str'),
-        period=dict(reuiqred=False,
-                    type='str',
-                    choices=['morning', 'noon', 'evening'],
-                    default='noon',
-                    )
-    )
-
-    period_phrases = {'morning': 'Good morning', 'noon': 'Hello', 'evening': 'Good evening'}
 
     module = AnsibleModule(
-                argument_spec=module_args,
-                supports_check_mode=True
+        argument_spec=dict(
+                name=dict(type='str', required=True),
+        )
     )
 
-    response = {"greet": period_phrases[module.params['period']] + ", " + module.params['name'] + "!"}
-    module.exit_json(changed=False, meta=response)
+    msg = "Hello, " + module.params['name']
+
+    response = {"msg": msg}
+    module.exit_json(changed=False, greeting=response)
 
 
 def main():
